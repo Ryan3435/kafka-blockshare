114e7e2d3cbcff5152741b15251cf8a8ab3f9287
Ryan3435 <rmshivers42@gmail.com>
2019-08-01 19:03:49 -0500

    Fixed setup script and cleaned it up.

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 apt-add-repository universe
 apt-get update
 apt-get install curl -y
@@ -7,6 +11,8 @@ apt-get install git -y
 apt-get install make -y
 apt-get install gcc -y
 
+
+echo "Installing Golang..."
 #Install Go
 wget https://golang.org/dl/go1.10.4.linux-amd64.tar.gz
 tar -xvzf go1.10.4.linux-amd64.tar.gz
@@ -20,12 +26,13 @@ echo "export GOPATH=$HOME/go" >> ~/.bashrc
 echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> ~/.bashrc
 echo "#END GO VARIABLES\n" >> ~/.bashrc
 
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+ export GOROOT=/usr/local/go
+ export GOPATH=$HOME/go
+ export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 go get -u golang.org/x/crypto/...
 
+echo "Installing docker..."
 #Install Docker
 curl  -fsSL  https://download.docker.com/linux/ubuntu/gpg  |  sudo  apt-key  add  -
 add-apt-repository  "deb  [arch=amd64]  https://download.docker.com/linux/ubuntu  $(lsb_release  -cs)  stable"
@@ -36,13 +43,17 @@ systemctl enable docker
 
 #Install Pip
 
+echo "Installing pip..."
 apt-get install python-pip -y
 
+
+echo "Installing docker compose..."
 #Install Docker Compose
 pip install docker-compose
 usermod -aG docker ${USER}
 chmod +x /usr/local/bin/docker-compose
 
+echo "Installing nodejs..."
 #Install nodejs and npm
 curl  -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get update
@@ -50,6 +61,7 @@ apt-get install nodejs -y
 npm install npm@5.6.0 -g
 
 #Install neccessary files for Hyperledger Fabric network
+echo "Installing tools..."
 git clone https://github.com/skcript/Kafka-Fabric-Network
 cd ./Kafka-Fabric-Network
 git checkout 0d1efa77ace5eac6bbc3195777188d9d0b1850d9
@@ -66,6 +78,7 @@ rm -r fabric-samples
 echo "\nexport PATH=$PATH:~/hlf/kafka-blockshare/bin" >> ~/.bashrc
 
 #Get a copy of fabric and move it to the appropriate directory
+echo "Cloning fabric..."
 git clone https://github.com/hyperledger/fabric.git
 cd ./fabric
 git checkout 306640d399bbea46d25bfa1673f35a3fa8187b49
@@ -73,6 +86,8 @@ cd ..
 mkdir -p $GOPATH/src/github.com/hyperledger
 mv -f fabric $GOPATH/src/github.com/hyperledger
 
+
+echo "Cloning fabric-sdk-go..."
 git clone https://github.com/hyperledger/fabric-sdk-go.git
 cd ./fabric-sdk-go
 git checkout 9efe90fcb75414fddddac02c653b169e46d3c33c
@@ -80,9 +95,13 @@ cd ..
 mv -f fabric-sdk-go $GOPATH/src/github.com/hyperledger
 /usr/local/go/bin/go get -u github.com/golang/dep/cmd/dep
 /usr/local/go/bin/go get -u github.com/hyperledger/fabric-sdk-go
+
+echo "Building fabric..."
 make -C $HOME/go/src/github.com/hyperledger/fabric-sdk-go
 make -C $HOME/go/src/github.com/hyperledger/fabric
 
+#Install qt5
+echo "Installing Qt..."
 apt-get install build-essential -y
 apt-get install qtcreator -y
 apt-get install build-essential -y
@@ -94,8 +113,12 @@ go get -u -v github.com/therecipe/qt/cmd/...
 $(go env GOPATH)/bin/qtsetup
 
 echo "export LD_LIBRARY_PATH=/home/ryan/go/src/github.com/therecipe/env_linux_amd64_513/5.13.0/gcc_64/lib/" >> ~/.bashrc
+cd $HOME/hlf/kafka-blockshare/client_applications
+echo "Building client application..."
+qtdeploy build
 
-#old
-#apt-get -y install libqt*5-dev qt*5-dev qt*5-doc-html qt5-default
-#go get -u -v -tags=no_env github.com/therecipe/qt/cmd/...
-#$(go env GOPATH)/bin/qtsetup
+echo "Starting blockchain network..."
+cd $HOME/hlf/kafka-blockshare
+./manage.sh all
+
+echo "Project installed and blockchain network started. To run client application run $HOME/hlf/kafka-blockshare/client_applications/deploy/linux/client_applications.  You must export LD_LIBRARY_PATH first, refer to this script or the README for path or use source ~/.bashrc"
